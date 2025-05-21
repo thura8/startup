d169502ca1178ed0614f50390f7e42720313d812
Thu Ra Lynn Htun <lynnhtunthura8@gmail.com>
2025-05-21 16:14:44 +0630

    Themeing and fonts and Hero section and search form

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,18 +15,18 @@ const config: Config = {
       },
       colors: {
         primary: {
-          100: "#FFE8F0",
+          "100": "#FFE8F0",
           DEFAULT: "#EE2B69",
         },
         secondary: "#FBE843",
         black: {
-          100: "#333333",
-          200: "#141413",
-          300: "#7D8087",
+          "100": "#333333",
+          "200": "#141413",
+          "300": "#7D8087",
           DEFAULT: "#000000",
         },
         white: {
-          100: "#F7F7F7",
+          "100": "#F7F7F7",
           DEFAULT: "#FFFFFF",
         },
       },
